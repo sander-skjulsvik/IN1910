0870875f3f1c387b5a9440e67d7a0acaae4a978d
MadBorg <rednas.1604@gmail.com>
2019-08-27 19:29:33 +0200

    Exercise 2d) Derivative of a polynomial, error in AddableDict.getitem, or may be somwhere else

--- Exercise2_A_class_for_general_polynomials.py
+++ Exercise2_A_class_for_general_polynomials.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 class Polynomial(object):
     def __init__(self, coeffs):
+        coeffs  = AddableDict(coeffs)
         self.coeffs = coeffs
 
     def __call__(self, x):
@@ -20,25 +21,57 @@ class Polynomial(object):
         return STR[:-3]
 
     def __add__(self, other):
-        coeffs = self.coeffs
-        new_coeffs = {}
-        #running through self.coeffs
+        coeffs = self.coeffs + other.coeffs
+        return Polynomial(coeffs)
+
+    def derivative(self):
+        coeffs = {}
         for exponent in self.coeffs:
-            #if exponent is in both coeffs add
-            if exponent in other.coeffs:
-                new_coeffs[exponent] =  self.coeffs[exponent] + other.coeffs[exponent]
-            #if exponent is only in self.coeffs, add
+            coeffs[exponent-1] = self.coeffs[exponent] * exponent
+        return Polynomial(coeffs)
+
+class AddableDict(dict):
+    def __init__(self, DICT):
+        self.DICT = DICT
+    
+    def __add__(self, other):
+        new_DICT = {}
+        #running through self.DICT
+        for key in self.DICT:
+            #if key is in both DICT add
+            if key in other.DICT:
+                new_DICT[key] =  self.DICT[key] + other.DICT[key]
+            #if key is only in self.DICT, add
             else:
-                new_coeffs[exponent] = self.coeffs[exponent]
-        #running through other.coeffs to catch the rest if the exponents
-        for exponent in other.coeffs:
-            #if exponent is only in other.coeffs, add
-            if exponent not in self.coeffs:
-                new_coeffs[exponent] = other.coeffs[exponent]
-        return Polynomial(new_coeffs)
+                new_DICT[key] = self.DICT[key]
+        #running through other.DICT to catch the rest if the exponents
+        for key in other.DICT:
+            #if key is only in other.DICT, add
+            if key not in self.DICT:
+                new_DICT[key] = other.DICT[key]
+        return  AddableDict(new_DICT)
+
+    def __str__(self):
+        return str(self.DICT)
+
+    def __getitem__(self, key):
+        return self.DICT[key]
 
 
+def test_AddableDict():
+    a = AddableDict({0: 2, 1: 3, 2: 4})
+    b = AddableDict({0: -1, 1:3, 2: 3, 3: 2})
+    c = a + b
+    assert c[0] == 1
+    assert c[1] == 6
+    assert c[2] == 7
+    assert c[3] == 2
 
+def test_derivative():
+    f = Polynomial({10:1, 6:-3, 2:2, 0:1})
+    f_deriv = f.derivative()
+    print(f'\n\nf = {f}, type f: {type(f)}, f_deriv = {f_deriv}, type f_deriv: {type(f_deriv)}\n\n')
+    assert f_deriv.coeffs == {9:10, 5:-18, 1:4}
 
 if __name__ == "__main__":
     # Exercise 2a) Defining the Polynomial class
@@ -52,7 +85,19 @@ if __name__ == "__main__":
     # plt.show()
 
     # Exercise 2b): Adding general polynomials together
-    f = Polynomial({0:1, 5:-7, 10:1})
-    g = Polynomial({5:7, 10:1, 15:-3})
-    
-    print(f+g)
+    # f = Polynomial({0:1, 5:-7, 10:1})
+    # g = Polynomial({5:7, 10:1, 15:-3})
+
+    # print(f+g)
+
+    # # Exercise 2c) Defining a AddableDictionary class
+    # a = AddableDict({0: 2, 1: 3, 2: 4})
+    # b = AddableDict({0: -1, 1:3, 2: 3, 3: 2})
+    # print(a + b)
+
+    # test_AddableDict()
+
+    # Exercise 2d) Derivative of a polynomial
+    test_derivative()   
+
+
