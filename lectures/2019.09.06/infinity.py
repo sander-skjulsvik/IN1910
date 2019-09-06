915dc37db5b9893b04149b32ee148b498b206d70
Sander Stokseth Skjulsvik <rednas.1604@gmail.com>
2019-09-06 11:36:03 +0200

    mass commit

---
+++ lectures/2019.09.06/infinity.py
@@ -0,0 +1,23 @@
+# import datetime
+# import time
+# import sys
+# x = []
+# i = 0
+# while True:
+#     try:
+#         x.append(datetime.datetime.now())
+#         # print(x[-1])
+#         # time.sleep()
+#         i += 1
+#         if i % 100:
+#             i = 0
+#             pass
+#     except:
+#         print('Not working')
+
+
+try:
+    while True:
+        pass
+except:
+    pass
