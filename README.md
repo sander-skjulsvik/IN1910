f96f7a4f988985bacc4bf2ddfa0c6c7b81b77b30
Sander Stokseth Skjulsvik <rednas.1604@gmail.com>
2019-10-29 10:24:26 +0100

    added url

---
+++ README.md
@@ -0,0 +1,2 @@
+# IN1910 - Programmering for naturvitenskapelige anvendelser
+https://www.uio.no/studier/emner/matnat/ifi/IN1910/h19/index.html
--- lectures/2019.10.29/mt19937.cpp
+++ lectures/2019.10.29/mt19937.cpp
@@ -4,10 +4,9 @@
 using namespace std;
 
 int main(){
-
+    
     // generating random numbers
     mt19937 engine{1234};
-    
     // converting the random numbers to the form i want
     uniform_real_distribution<float> uniform(0,1);
 
