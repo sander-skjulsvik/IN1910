5880ce7808028f9d8b68a9c811a73d1577d8531b
Sander Stokseth Skjulsvik <rednas.1604@gmail.com>
2019-11-07 09:36:32 +0100

    vectorized 1D walkers with print

--- tasks/week10/notes.md
+++ tasks/week10/notes.md
@@ -1,10 +1,12 @@
 # Task week 10 - Random walks and Marcov Processes
 
 ## Task 1: Root Mean Square Displacement of a random Walker
-    From lecture:
-        Average dispplacement of a balanced random walker is
-```math
-        X_{i} = 20
-```
+    Average dispplacement of a balanced random walker is
+        <X_n> = 0
+    in both 1D and 2D. This is bec because the walker has equal changes to go in either direction.
+    However, the averaged root mean square displacement, is not 0, it is
+        sqrt(<X_n^2>) = sqrt(n), 1-d.
 
+### a: Performing random walks
+    Use np.random.randint and np.cumsum to perform 1000 random walks of 500 steps each.
             
---
+++ tasks/week10/rand_walks.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def walker_1D(N = 20, X0 = 0, show=True):
+    X = np.empty(N+1)
+    X[0] = X0
+    for i in range(N):
+        X[i+1] = X[i] + 2*np.random.randint(2) -1
+    if show:
+        plot(X)
+
+def walkers_1D_vectorized(N = 20, M = 2, X0 = 0, plot_path=True):
+    X = np.empty((N, M)) 
+    X[0] = X0 # init val
+    K = 2*np.random.randint(2, size=(N-1, M)) - 1 # step
+    X[1:, :] = X[0, :] + np.cumsum(K, axis=0) # vectorized sum
+
+    if plot_path is True:
+        plot(X)
+
+def plot(X, s = None):
+    y = X
+    x = range(len(y))
+    fig, ax = plt.subplots()
+    ax.step(x,y, where="mid", alpha=0.02, color="g")
+    ax.set_xlabel(" Nr of steps taken")
+    ax.set_ylabel(" Displacement ")
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    num_walkers = M = 1000
+    num_steps = N = 500
+    # walker_1D(num_steps)
+    walkers_1D_vectorized(N, M)
+
+
